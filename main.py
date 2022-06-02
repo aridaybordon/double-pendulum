23cc527a60d80378ee0abe6c2beeb54ed4f95907
Aridai Bordón <aridaybordon@gmail.com>
2022-06-02 09:31:31 +0100

    fix: upgrade Pillow

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from scripts.animation import create_animation_from_prediction
 from scripts.training import make_and_save_prediction, train_model
 
 
-def main() -> None:
-    # ========================================================================
-    #    Load model, train it with newly created data and simulate movement
-    # ========================================================================
-
+def main():
+    """
+    Load model, train it with newly created data and simulate movement.
+    
+    """
     model = train_model(load_weights=False, create_data=True)
 
     # Draw NN prediction
     z0 = [3, 4, 0, 0] # Initial conditions
     make_and_save_prediction(model, z0)
-    create_animation_from_prediction(f"data/generated_movement/DP_train.gif")
+    create_animation_from_prediction(filename=f"data/generated_movement/DP_train.gif")
 
 
 if __name__ == '__main__':
